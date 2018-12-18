d02240a71fa30d73deb709a609442ad233ee453f
MeeraanSha <42766518+MeeraanSha@users.noreply.github.com>
2018-12-18 11:38:13 +0530

    Update codekata3.C

--- codekata3.C
+++ codekata3.C
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int main()
+void main()
 {
 char c;
-int isLowercaseVowel, isUppercaseVowel;
 scanf("%c",&c);
-isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-isLowercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-if (isLowercaseVowel || isUppercaseVowel)
-printf("vowel c");
+if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+{
+printf("it s a vowel");
+}
 else
-printf("consonant c");
-return 0;
+{
+printf("it is a consonant");
+}
 }
