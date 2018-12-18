5356fc7192ea80185dca9c3b784019496f83fffe
MeeraanSha <42766518+MeeraanSha@users.noreply.github.com>
2018-12-18 12:38:15 +0530

    Update codekata3.C

--- codekata3.C
+++ codekata3.C
@@ -1,15 +1,12 @@
-#include <stdio.h>
+#include<stdio.h>
 void main()
 {
 char c;
-printf("enter the character:");
 scanf("%c",&c);
-if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-{
-printf("it s a vowel");
-}
-else
-{
-printf("it is a consonant");
-}
+  if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+  {
+    printf("vowel");
+  }
+  else
+    printf("consonant");
 }
